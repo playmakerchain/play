2188438b17fc88eabfa57b3ce0522c273c236217
realchainlife <realchainlife@protonmail.com>
2019-03-31 14:51:15 +0300

    edit import linker >> powerplay

--- genesis/testnet.go
+++ genesis/testnet.go
@@ -9,11 +9,11 @@ package genesis
 import (
 	"math/big"
 
-	"github.com/playmakerchain//builtin"
-	"github.com/playmakerchain//state"
-	"github.com/playmakerchain//"
-	"github.com/playmakerchain//tx"
-	"github.com/playmakerchain//vm"
+	"github.com/playmakerchain/powerplay/builtin"
+	"github.com/playmakerchain/powerplay/state"
+	"github.com/playmakerchain/powerplay/powerplay"
+	"github.com/playmakerchain/powerplay/tx"
+	"github.com/playmakerchain/powerplay/vm"
 )
 
 // NewTestnet create genesis for testnet.
@@ -21,21 +21,21 @@ func NewTestnet() *Genesis {
 	launchTime := uint64(1530014400) // 'Tue Jun 26 2018 20:00:00 GMT+0800 (CST)'
 
 	// use this address as executor instead of builtin one, for test purpose
-	executor, _ := .ParseAddress("0xB5A34b62b63A6f1EE99DFD30b133B657859f8d79")
-	acccount0, _ := .ParseAddress("0xe59D475Abe695c7f67a8a2321f33A856B0B4c71d")
+	executor, _ := powerplay.ParseAddress("0xB5A34b62b63A6f1EE99DFD30b133B657859f8d79")
+	acccount0, _ := powerplay.ParseAddress("0xe59D475Abe695c7f67a8a2321f33A856B0B4c71d")
 
-	master0, _ := .ParseAddress("0x25AE0ef84dA4a76D5a1DFE80D3789C2c46FeE30a")
-	endorser0, _ := .ParseAddress("0xb4094c25f86d628fdD571Afc4077f0d0196afB48")
+	master0, _ := powerplay.ParseAddress("0x25AE0ef84dA4a76D5a1DFE80D3789C2c46FeE30a")
+	endorser0, _ := powerplay.ParseAddress("0xb4094c25f86d628fdD571Afc4077f0d0196afB48")
 
 	builder := new(Builder).
 		Timestamp(launchTime).
-		GasLimit(.InitialGasLimit).
+		GasLimit(powerplay.InitialGasLimit).
 		State(func(state *state.State) error {
 			tokenSupply := new(big.Int)
 
 			// alloc precompiled contracts
 			for addr := range vm.PrecompiledContractsByzantium {
-				state.SetCode(.Address(addr), emptyRuntimeBytecode)
+				state.SetCode(powerplay.Address(addr), emptyRuntimeBytecode)
 			}
 
 			// setup builtin contracts
@@ -63,19 +63,19 @@ func NewTestnet() *Genesis {
 		// set initial params
 		// use an external account as executor to manage testnet easily
 		Call(
-			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", .KeyExecutorAddress, new(big.Int).SetBytes(executor[:]))),
-			.Address{}).
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", powerplay.KeyExecutorAddress, new(big.Int).SetBytes(executor[:]))),
+			powerplay.Address{}).
 		Call(
-			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", .KeyRewardRatio, .InitialRewardRatio)),
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", powerplay.KeyRewardRatio, powerplay.InitialRewardRatio)),
 			executor).
 		Call(
-			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", .KeyBaseGasPrice, .InitialBaseGasPrice)),
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", powerplay.KeyBaseGasPrice, powerplay.InitialBaseGasPrice)),
 			executor).
 		Call(
-			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", .KeyProposerEndorsement, .InitialProposerEndorsement)),
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", powerplay.KeyProposerEndorsement, powerplay.InitialProposerEndorsement)),
 			executor).
 		// add master0 as the initial block proposer
-		Call(tx.NewClause(&builtin.Authority.Address).WithData(mustEncodeInput(builtin.Authority.ABI, "add", master0, endorser0, .BytesToBytes32([]byte("master0")))),
+		Call(tx.NewClause(&builtin.Authority.Address).WithData(mustEncodeInput(builtin.Authority.ABI, "add", master0, endorser0, powerplay.BytesToBytes32([]byte("master0")))),
 			executor)
 
 	id, err := builder.ComputeID()
