a54992d3093639b646dcd41ba676af9c314191b0
realchainlife <realchainlife@protonmail.com>
2019-04-01 11:50:11 +0300

    add: go fork chain file

---
+++ chain/fork.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The VeChainThor developers
+// Copyright (c) 2019 THe PlayMaker developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package chain
+
+import (
+	"github.com/vechain/thor/block"
+)
+
+// Fork describes forked chain.
+type Fork struct {
+	Ancestor *block.Header
+	Trunk    []*block.Header
+	Branch   []*block.Header
+}
