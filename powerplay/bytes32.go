b353985d89654c463ae787a834d16697d4c5e4d8
realchainlife <realchainlife@protonmail.com>
2019-04-02 10:39:20 +0300

    change package to powerplay

--- powerplay/bytes32.go
+++ powerplay/bytes32.go
@@ -4,7 +4,7 @@
 // Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
 // file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
 
-package 
+package powerplay
 
 import (
 	"encoding/hex"
