5ffc823476a30c912c9f0b370807d774ed2b3086
realchainlife <realchainlife@protonmail.com>
2019-04-03 09:46:56 +0300

    change: import linker >>> playmakerchain

--- state/cached_object.go
+++ state/cached_object.go
@@ -8,8 +8,8 @@ package state
 
 import (
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/vechain/thor/kv"
-	"github.com/vechain/thor/thor"
+	"github.com//thor/kv"
+	"github.com//thor/thor"
 )
 
 // cachedObject to cache code and storage of an account.
