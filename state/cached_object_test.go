81910301a70f0d1bb833bca2724b27b30985d8ef
realchainlife <realchainlife@protonmail.com>
2019-04-03 09:55:56 +0300

    edit: import linker >>> playmakerchain

--- state/cached_object_test.go
+++ state/cached_object_test.go
@@ -14,9 +14,9 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/stretchr/testify/assert"
-	"github.com/vechain/thor/lvldb"
-	"github.com/vechain/thor/thor"
-	"github.com/vechain/thor/trie"
+	"github.com//thor/lvldb"
+	"github.com//thor/thor"
+	"github.com//thor/trie"
 )
 
 func TestCachedObject(t *testing.T) {
