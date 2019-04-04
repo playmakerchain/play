ca78bda429540f09f92378b65cf5e90d66c079db
realchainlife <realchainlife@protonmail.com>
2019-04-04 10:23:43 +0300

    modify: import linker >>> powerplay

--- builtin/params/params.go
+++ builtin/params/params.go
@@ -10,22 +10,22 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/playmakerchain/thor/state"
-	"github.com/playmakerchain/thor/thor"
+	"github.com/playmakerchain//state"
+	"github.com/playmakerchain//"
 )
 
 // Params binder of `Params` contract.
 type Params struct {
-	addr  thor.Address
+	addr  .Address
 	state *state.State
 }
 
-func New(addr thor.Address, state *state.State) *Params {
+func New(addr .Address, state *state.State) *Params {
 	return &Params{addr, state}
 }
 
 // Get native way to get param.
-func (p *Params) Get(key thor.Bytes32) (value *big.Int) {
+func (p *Params) Get(key .Bytes32) (value *big.Int) {
 	p.state.DecodeStorage(p.addr, key, func(raw []byte) error {
 		if len(raw) == 0 {
 			value = &big.Int{}
@@ -37,7 +37,7 @@ func (p *Params) Get(key thor.Bytes32) (value *big.Int) {
 }
 
 // Set native way to set param.
-func (p *Params) Set(key thor.Bytes32, value *big.Int) {
+func (p *Params) Set(key .Bytes32, value *big.Int) {
 	p.state.EncodeStorage(p.addr, key, func() ([]byte, error) {
 		if value.Sign() == 0 {
 			return nil, nil
