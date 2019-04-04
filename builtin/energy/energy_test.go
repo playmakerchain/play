e4972913fbfcb7d37203fd257ad4a4aaee73c0a0
realchainlife <realchainlife@protonmail.com>
2019-04-04 09:57:06 +0300

    modify: import file linker >>> powerplay

--- builtin/energy/energy_test.go
+++ builtin/energy/energy_test.go
@@ -11,18 +11,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/playmakerchain/thor/lvldb"
-	"github.com/playmakerchain/thor/state"
-	"github.com/playmakerchain/thor/thor"
+	"github.com/playmakerchain//lvldb"
+	"github.com/playmakerchain//state"
+	"github.com/playmakerchain//"
 )
 
 func TestEnergy(t *testing.T) {
 	kv, _ := lvldb.NewMem()
-	st, _ := state.New(thor.Bytes32{}, kv)
+	st, _ := state.New(.Bytes32{}, kv)
 
-	acc := thor.BytesToAddress([]byte("a1"))
+	acc := .BytesToAddress([]byte("a1"))
 
-	eng := New(thor.BytesToAddress([]byte("eng")), st, 0)
+	eng := New(.BytesToAddress([]byte("eng")), st, 0)
 	tests := []struct {
 		ret      interface{}
 		expected interface{}
@@ -43,19 +43,19 @@ func TestEnergy(t *testing.T) {
 
 func TestEnergyGrowth(t *testing.T) {
 	kv, _ := lvldb.NewMem()
-	st, _ := state.New(thor.Bytes32{}, kv)
+	st, _ := state.New(.Bytes32{}, kv)
 
-	acc := thor.BytesToAddress([]byte("a1"))
+	acc := .BytesToAddress([]byte("a1"))
 
 	st.SetEnergy(acc, &big.Int{}, 10)
 
 	vetBal := big.NewInt(1e18)
 	st.SetBalance(acc, vetBal)
 
-	bal1 := New(thor.Address{}, st, 1000).
+	bal1 := New(.Address{}, st, 1000).
 		Get(acc)
 
-	x := new(big.Int).Mul(thor.EnergyGrowthRate, vetBal)
+	x := new(big.Int).Mul(.EnergyGrowthRate, vetBal)
 	x.Mul(x, new(big.Int).SetUint64(1000-10))
 	x.Div(x, big.NewInt(1e18))
 
