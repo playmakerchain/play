980300cb28d4ff9d54fbd0b80942a2adaa09f38a
realchainlife <realchainlife@protonmail.com>
2019-04-04 09:55:17 +0300

    modify: import file linker >>> playmakerchain

---
+++ builtin/energy/types.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 The VeChainThor developers
+// Copyright (c) 2019 The PlayMaker developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package energy
+
+import (
+	"math/big"
+)
+
+type (
+	initialSupply struct {
+		Token     *big.Int
+		Energy    *big.Int
+		BlockTime uint64
+	}
+	totalAddSub struct {
+		TotalAdd *big.Int
+		TotalSub *big.Int
+	}
+)
