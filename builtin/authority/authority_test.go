285cf54c041dbb4c9b9d5d77f030ccf905fb23fe
realchainlife <realchainlife@protonmail.com>
2019-04-04 09:51:58 +0300

    modify: import file linker >>> powerplay

--- builtin/authority/authority_test.go
+++ builtin/authority/authority_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/playmakerchain/thor/lvldb"
-	"github.com/playmakerchain/thor/state"
-	"github.com/playmakerchain/thor/thor"
+	"github.com/playmakerchain//lvldb"
+	"github.com/playmakerchain//state"
+	"github.com/playmakerchain//"
 )
 
 func M(a ...interface{}) []interface{} {
@@ -22,46 +22,46 @@ func M(a ...interface{}) []interface{} {
 
 func TestAuthority(t *testing.T) {
 	kv, _ := lvldb.NewMem()
-	st, _ := state.New(thor.Bytes32{}, kv)
+	st, _ := state.New(.Bytes32{}, kv)
 
-	p1 := thor.BytesToAddress([]byte("p1"))
-	p2 := thor.BytesToAddress([]byte("p2"))
-	p3 := thor.BytesToAddress([]byte("p3"))
+	p1 := .BytesToAddress([]byte("p1"))
+	p2 := .BytesToAddress([]byte("p2"))
+	p3 := .BytesToAddress([]byte("p3"))
 
 	st.SetBalance(p1, big.NewInt(10))
 	st.SetBalance(p2, big.NewInt(20))
 	st.SetBalance(p3, big.NewInt(30))
 
-	aut := New(thor.BytesToAddress([]byte("aut")), st)
+	aut := New(.BytesToAddress([]byte("aut")), st)
 	tests := []struct {
 		ret      interface{}
 		expected interface{}
 	}{
-		{aut.Add(p1, p1, thor.Bytes32{}), true},
-		{M(aut.Get(p1)), []interface{}{true, p1, thor.Bytes32{}, true}},
-		{aut.Add(p2, p2, thor.Bytes32{}), true},
-		{aut.Add(p3, p3, thor.Bytes32{}), true},
-		{M(aut.Candidates(big.NewInt(10), thor.MaxBlockProposers)), []interface{}{
-			[]*Candidate{{p1, p1, thor.Bytes32{}, true}, {p2, p2, thor.Bytes32{}, true}, {p3, p3, thor.Bytes32{}, true}},
+		{aut.Add(p1, p1, .Bytes32{}), true},
+		{M(aut.Get(p1)), []interface{}{true, p1, .Bytes32{}, true}},
+		{aut.Add(p2, p2, .Bytes32{}), true},
+		{aut.Add(p3, p3, .Bytes32{}), true},
+		{M(aut.Candidates(big.NewInt(10), .MaxBlockProposers)), []interface{}{
+			[]*Candidate{{p1, p1, .Bytes32{}, true}, {p2, p2, .Bytes32{}, true}, {p3, p3, .Bytes32{}, true}},
 		}},
-		{M(aut.Candidates(big.NewInt(20), thor.MaxBlockProposers)), []interface{}{
-			[]*Candidate{{p2, p2, thor.Bytes32{}, true}, {p3, p3, thor.Bytes32{}, true}},
+		{M(aut.Candidates(big.NewInt(20), .MaxBlockProposers)), []interface{}{
+			[]*Candidate{{p2, p2, .Bytes32{}, true}, {p3, p3, .Bytes32{}, true}},
 		}},
-		{M(aut.Candidates(big.NewInt(30), thor.MaxBlockProposers)), []interface{}{
-			[]*Candidate{{p3, p3, thor.Bytes32{}, true}},
+		{M(aut.Candidates(big.NewInt(30), .MaxBlockProposers)), []interface{}{
+			[]*Candidate{{p3, p3, .Bytes32{}, true}},
 		}},
 		{M(aut.Candidates(big.NewInt(10), 2)), []interface{}{
-			[]*Candidate{{p1, p1, thor.Bytes32{}, true}, {p2, p2, thor.Bytes32{}, true}},
+			[]*Candidate{{p1, p1, .Bytes32{}, true}, {p2, p2, .Bytes32{}, true}},
 		}},
-		{M(aut.Get(p1)), []interface{}{true, p1, thor.Bytes32{}, true}},
+		{M(aut.Get(p1)), []interface{}{true, p1, .Bytes32{}, true}},
 		{aut.Update(p1, false), true},
-		{M(aut.Get(p1)), []interface{}{true, p1, thor.Bytes32{}, false}},
+		{M(aut.Get(p1)), []interface{}{true, p1, .Bytes32{}, false}},
 		{aut.Update(p1, true), true},
-		{M(aut.Get(p1)), []interface{}{true, p1, thor.Bytes32{}, true}},
+		{M(aut.Get(p1)), []interface{}{true, p1, .Bytes32{}, true}},
 		{aut.Revoke(p1), true},
-		{M(aut.Get(p1)), []interface{}{false, p1, thor.Bytes32{}, false}},
-		{M(aut.Candidates(&big.Int{}, thor.MaxBlockProposers)), []interface{}{
-			[]*Candidate{{p2, p2, thor.Bytes32{}, true}, {p3, p3, thor.Bytes32{}, true}},
+		{M(aut.Get(p1)), []interface{}{false, p1, .Bytes32{}, false}},
+		{M(aut.Candidates(&big.Int{}, .MaxBlockProposers)), []interface{}{
+			[]*Candidate{{p2, p2, .Bytes32{}, true}, {p3, p3, .Bytes32{}, true}},
 		}},
 	}
 
