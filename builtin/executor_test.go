d58bda60d3db4133ab0a996e14a0e3a4e09d73fe
realchainlife <realchainlife@protonmail.com>
2019-04-05 14:22:03 +0300

    modify: import linker >>> powerplay

--- builtin/executor_test.go
+++ builtin/executor_test.go
@@ -15,47 +15,47 @@ import (
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
-	"github.com/playmakerchain/thor/builtin"
-	"github.com/playmakerchain/thor/chain"
-	"github.com/playmakerchain/thor/lvldb"
-	"github.com/playmakerchain/thor/runtime"
-	"github.com/playmakerchain/thor/state"
-	"github.com/playmakerchain/thor/thor"
-	"github.com/playmakerchain/thor/tx"
-	"github.com/playmakerchain/thor/xenv"
+	"github.com/playmakerchain//builtin"
+	"github.com/playmakerchain//chain"
+	"github.com/playmakerchain//lvldb"
+	"github.com/playmakerchain//runtime"
+	"github.com/playmakerchain//state"
+	"github.com/playmakerchain//"
+	"github.com/playmakerchain//tx"
+	"github.com/playmakerchain//xenv"
 )
 
-func approverEvent(approver thor.Address, action string) *tx.Event {
+func approverEvent(approver .Address, action string) *tx.Event {
 	ev, _ := builtin.Executor.ABI.EventByName("Approver")
-	var b32 thor.Bytes32
+	var b32 .Bytes32
 	copy(b32[:], action)
 	data, _ := ev.Encode(b32)
 	return &tx.Event{
 		Address: builtin.Executor.Address,
-		Topics:  []thor.Bytes32{ev.ID(), thor.BytesToBytes32(approver.Bytes())},
+		Topics:  [].Bytes32{ev.ID(), .BytesToBytes32(approver.Bytes())},
 		Data:    data,
 	}
 }
-func proposalEvent(id thor.Bytes32, action string) *tx.Event {
+func proposalEvent(id .Bytes32, action string) *tx.Event {
 	ev, _ := builtin.Executor.ABI.EventByName("Proposal")
-	var b32 thor.Bytes32
+	var b32 .Bytes32
 	copy(b32[:], action)
 	data, _ := ev.Encode(b32)
 	return &tx.Event{
 		Address: builtin.Executor.Address,
-		Topics:  []thor.Bytes32{ev.ID(), id},
+		Topics:  [].Bytes32{ev.ID(), id},
 		Data:    data,
 	}
 }
 
-func votingContractEvent(addr thor.Address, action string) *tx.Event {
+func votingContractEvent(addr .Address, action string) *tx.Event {
 	ev, _ := builtin.Executor.ABI.EventByName("VotingContract")
-	var b32 thor.Bytes32
+	var b32 .Bytes32
 	copy(b32[:], action)
 	data, _ := ev.Encode(b32)
 	return &tx.Event{
 		Address: builtin.Executor.Address,
-		Topics:  []thor.Bytes32{ev.ID(), thor.BytesToBytes32(addr.Bytes())},
+		Topics:  [].Bytes32{ev.ID(), .BytesToBytes32(addr.Bytes())},
 		Data:    data,
 	}
 }
@@ -66,7 +66,7 @@ func initExectorTest() *ctest {
 		state.SetCode(builtin.Prototype.Address, builtin.Prototype.RuntimeBytecodes())
 		state.SetCode(builtin.Executor.Address, builtin.Executor.RuntimeBytecodes())
 		state.SetCode(builtin.Params.Address, builtin.Params.RuntimeBytecodes())
-		builtin.Params.Native(state).Set(thor.KeyExecutorAddress, new(big.Int).SetBytes(builtin.Executor.Address[:]))
+		builtin.Params.Native(state).Set(.KeyExecutorAddress, new(big.Int).SetBytes(builtin.Executor.Address[:]))
 		return nil
 	})
 
@@ -85,23 +85,23 @@ func initExectorTest() *ctest {
 
 func TestExecutorApprover(t *testing.T) {
 	test := initExectorTest()
-	var approvers []thor.Address
+	var approvers [].Address
 	for i := 0; i < 7; i++ {
-		approvers = append(approvers, thor.BytesToAddress([]byte(fmt.Sprintf("approver%d", i))))
+		approvers = append(approvers, .BytesToAddress([]byte(fmt.Sprintf("approver%d", i))))
 	}
 
 	for _, a := range approvers {
 		// zero identity
-		test.Case("addApprover", a, thor.Bytes32{}).
+		test.Case("addApprover", a, .Bytes32{}).
 			ShouldVMError(errReverted).
 			Assert(t)
 
-		test.Case("addApprover", a, thor.BytesToBytes32(a.Bytes())).
-			Caller(thor.BytesToAddress([]byte("other"))).
+		test.Case("addApprover", a, .BytesToBytes32(a.Bytes())).
+			Caller(.BytesToAddress([]byte("other"))).
 			ShouldVMError(errReverted).
 			Assert(t)
 
-		test.Case("addApprover", a, thor.BytesToBytes32(a.Bytes())).
+		test.Case("addApprover", a, .BytesToBytes32(a.Bytes())).
 			Caller(builtin.Executor.Address).
 			ShouldLog(approverEvent(a, "added")).
 			Assert(t)
@@ -112,14 +112,14 @@ func TestExecutorApprover(t *testing.T) {
 		ShouldOutput(uint8(len(approvers))).
 		Assert(t)
 
-	test.Case("addApprover", approvers[0], thor.BytesToBytes32(approvers[0].Bytes())).
+	test.Case("addApprover", approvers[0], .BytesToBytes32(approvers[0].Bytes())).
 		Caller(builtin.Executor.Address).
 		ShouldVMError(errReverted).
 		Assert(t)
 
 	for _, a := range approvers {
 		test.Case("approvers", a).
-			ShouldOutput(thor.BytesToBytes32(a.Bytes()), true).
+			ShouldOutput(.BytesToBytes32(a.Bytes()), true).
 			Assert(t)
 	}
 
@@ -142,7 +142,7 @@ func TestExecutorApprover(t *testing.T) {
 func TestExecutorVotingContract(t *testing.T) {
 
 	test := initExectorTest()
-	voting := thor.BytesToAddress([]byte("voting"))
+	voting := .BytesToAddress([]byte("voting"))
 	test.Case("attachVotingContract", voting).
 		ShouldVMError(errReverted).
 		Assert(t)
@@ -179,20 +179,20 @@ func TestExecutorProposal(t *testing.T) {
 
 	target := builtin.Params.Address
 	setParam, _ := builtin.Params.ABI.MethodByName("set")
-	data, _ := setParam.EncodeInput(thor.BytesToBytes32([]byte("paramKey")), big.NewInt(12345))
+	data, _ := setParam.EncodeInput(.BytesToBytes32([]byte("paramKey")), big.NewInt(12345))
 	test.Case("propose", target, data).
 		ShouldVMError(errReverted).
 		Assert(t)
 
-	approver := thor.BytesToAddress([]byte("approver"))
-	test.Case("addApprover", approver, thor.BytesToBytes32(approver.Bytes())).
+	approver := .BytesToAddress([]byte("approver"))
+	test.Case("addApprover", approver, .BytesToBytes32(approver.Bytes())).
 		Caller(builtin.Executor.Address).
 		Assert(t)
 
-	proposalID := func() thor.Bytes32 {
+	proposalID := func() .Bytes32 {
 		var b8 [8]byte
 		binary.BigEndian.PutUint64(b8[:], test.rt.Context().Time)
-		return thor.Bytes32(crypto.Keccak256Hash(b8[:], approver[:]))
+		return .Bytes32(crypto.Keccak256Hash(b8[:], approver[:]))
 	}()
 	test.Case("propose", target, data).
 		Caller(approver).
@@ -252,5 +252,5 @@ func TestExecutorProposal(t *testing.T) {
 			data).
 		Assert(t)
 
-	assert.Equal(t, builtin.Params.Native(test.rt.State()).Get(thor.BytesToBytes32([]byte("paramKey"))), big.NewInt(12345))
+	assert.Equal(t, builtin.Params.Native(test.rt.State()).Get(.BytesToBytes32([]byte("paramKey"))), big.NewInt(12345))
 }
