d830dd0e7668fcbe95a5db174860fd5c93352ff0
realchainlife <realchainlife@protonmail.com>
2019-04-06 16:55:52 +0300

    add: entry cache file

---
+++ cache/entry.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 The VeChainThor developers
+// Copyright (c) 2019 THe PlayMaker developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package cache
+
+// Entry entry of cache.
+type Entry struct {
+	Key, Value interface{}
+}
