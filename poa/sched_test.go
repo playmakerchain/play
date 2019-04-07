6cb6094c5777029ccdbc971721fc0dad9f22475e
realchainlife <realchainlife@protonmail.com>
2019-04-07 12:54:33 +0300

    modify: import linker >>> powerplay

--- poa/sched_test.go
+++ poa/sched_test.go
@@ -10,16 +10,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/playmakerchain/thor/poa"
-	"github.com/playmakerchain/thor/thor"
+	"github.com/playmakerchain//poa"
+	"github.com/playmakerchain//"
 )
 
 var (
-	p1 = thor.BytesToAddress([]byte("p1"))
-	p2 = thor.BytesToAddress([]byte("p2"))
-	p3 = thor.BytesToAddress([]byte("p3"))
-	p4 = thor.BytesToAddress([]byte("p4"))
-	p5 = thor.BytesToAddress([]byte("p5"))
+	p1 = .BytesToAddress([]byte("p1"))
+	p2 = .BytesToAddress([]byte("p2"))
+	p3 = .BytesToAddress([]byte("p3"))
+	p4 = .BytesToAddress([]byte("p4"))
+	p5 = .BytesToAddress([]byte("p5"))
 
 	proposers = []poa.Proposer{
 		{p1, false},
@@ -34,13 +34,13 @@ var (
 
 func TestSchedule(t *testing.T) {
 
-	_, err := poa.NewScheduler(thor.BytesToAddress([]byte("px")), proposers, 1, parentTime)
+	_, err := poa.NewScheduler(.BytesToAddress([]byte("px")), proposers, 1, parentTime)
 	assert.NotNil(t, err)
 
 	sched, _ := poa.NewScheduler(p1, proposers, 1, parentTime)
 
 	for i := uint64(0); i < 100; i++ {
-		now := parentTime + i*thor.BlockInterval/2
+		now := parentTime + i*.BlockInterval/2
 		nbt := sched.Schedule(now)
 		assert.True(t, nbt >= now)
 		assert.True(t, sched.IsTheTime(nbt))
@@ -55,8 +55,8 @@ func TestIsTheTime(t *testing.T) {
 		want bool
 	}{
 		{parentTime - 1, false},
-		{parentTime + thor.BlockInterval/2, false},
-		{parentTime + thor.BlockInterval, true},
+		{parentTime + .BlockInterval/2, false},
+		{parentTime + .BlockInterval, true},
 	}
 
 	for _, tt := range tests {
@@ -72,8 +72,8 @@ func TestUpdates(t *testing.T) {
 		newBlockTime uint64
 		want         uint64
 	}{
-		{parentTime + thor.BlockInterval, 2},
-		{parentTime + thor.BlockInterval*30, 1},
+		{parentTime + .BlockInterval, 2},
+		{parentTime + .BlockInterval*30, 1},
 	}
 
 	for _, tt := range tests {
