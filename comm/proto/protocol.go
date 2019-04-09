4ba34ff7bf97a30fdfb80982e87fb103f327b953
realchainlife <realchainlife@protonmail.com>
2019-04-09 09:46:34 +0300

    modify: import linker >>> powerplay

--- comm/proto/protocol.go
+++ comm/proto/protocol.go
@@ -12,13 +12,13 @@ import (
 
 // Constants
 const (
-	Name              = ""
+	Name              = "powerplay"
 	Version    uint   = 1
 	Length     uint64 = 8
 	MaxMsgSize        = 10 * 1024 * 1024
 )
 
-// Protocol messages of 
+// Protocol messages of powerplay
 const (
 	MsgGetStatus = iota
 	MsgNewBlockID
