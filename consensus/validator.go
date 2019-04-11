35e121810895a0b9052f55724d6b2b17fda197ea
realchainlife <realchainlife@protonmail.com>
2019-04-11 11:23:49 +0300

    modify: import linker >>> powerplay

--- consensus/validator.go
+++ consensus/validator.go
@@ -10,14 +10,14 @@ import (
 	"fmt"
 
 	"github.com/pkg/errors"
-	"github.com/playmakerchain/thor/block"
-	"github.com/playmakerchain/thor/builtin"
-	"github.com/playmakerchain/thor/poa"
-	"github.com/playmakerchain/thor/runtime"
-	"github.com/playmakerchain/thor/state"
-	"github.com/playmakerchain/thor/thor"
-	"github.com/playmakerchain/thor/tx"
-	"github.com/playmakerchain/thor/xenv"
+	"github.com/playmakerchain//block"
+	"github.com/playmakerchain//builtin"
+	"github.com/playmakerchain//poa"
+	"github.com/playmakerchain//runtime"
+	"github.com/playmakerchain//state"
+	"github.com/playmakerchain//"
+	"github.com/playmakerchain//tx"
+	"github.com/playmakerchain//xenv"
 )
 
 func (c *Consensus) validate(
@@ -53,11 +53,11 @@ func (c *Consensus) validateBlockHeader(header *block.Header, parent *block.Head
 		return consensusError(fmt.Sprintf("block timestamp behind parents: parent %v, current %v", parent.Timestamp(), header.Timestamp()))
 	}
 
-	if (header.Timestamp()-parent.Timestamp())%thor.BlockInterval != 0 {
+	if (header.Timestamp()-parent.Timestamp())%.BlockInterval != 0 {
 		return consensusError(fmt.Sprintf("block interval not rounded: parent %v, current %v", parent.Timestamp(), header.Timestamp()))
 	}
 
-	if header.Timestamp() > nowTimestamp+thor.BlockInterval {
+	if header.Timestamp() > nowTimestamp+.BlockInterval {
 		return errFutureBlock
 	}
 
@@ -83,9 +83,9 @@ func (c *Consensus) validateProposer(header *block.Header, parent *block.Header,
 	}
 
 	authority := builtin.Authority.Native(st)
-	endorsement := builtin.Params.Native(st).Get(thor.KeyProposerEndorsement)
+	endorsement := builtin.Params.Native(st).Get(.KeyProposerEndorsement)
 
-	candidates := authority.Candidates(endorsement, thor.MaxBlockProposers)
+	candidates := authority.Candidates(endorsement, .MaxBlockProposers)
 	proposers := make([]poa.Proposer, 0, len(candidates))
 	for _, c := range candidates {
 		proposers = append(proposers, poa.Proposer{
@@ -146,7 +146,7 @@ func (c *Consensus) verifyBlock(blk *block.Block, state *state.State) (*state.St
 	var totalGasUsed uint64
 	txs := blk.Transactions()
 	receipts := make(tx.Receipts, 0, len(txs))
-	processedTxs := make(map[thor.Bytes32]bool)
+	processedTxs := make(map[.Bytes32]bool)
 	header := blk.Header()
 	signer, _ := header.Signer()
 	rt := runtime.New(
@@ -161,7 +161,7 @@ func (c *Consensus) verifyBlock(blk *block.Block, state *state.State) (*state.St
 			TotalScore:  header.TotalScore(),
 		})
 
-	findTx := func(txID thor.Bytes32) (found bool, reverted bool, err error) {
+	findTx := func(txID .Bytes32) (found bool, reverted bool, err error) {
 		if reverted, ok := processedTxs[txID]; ok {
 			return true, reverted, nil
 		}
