1afd0ba4e43be7e829aae154ff2b62e383b07fee
realchainlife <realchainlife@protonmail.com>
2019-04-11 12:00:27 +0300

    modify: import linker >>> playmakerchain

--- runtime/resolved_tx_test.go
+++ runtime/resolved_tx_test.go
@@ -16,14 +16,14 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/stretchr/testify/assert"
-	"github.com//thor/builtin"
-	"github.com//thor/chain"
-	"github.com//thor/genesis"
-	"github.com//thor/lvldb"
-	"github.com//thor/runtime"
-	"github.com//thor/state"
-	"github.com//thor/thor"
-	"github.com//thor/tx"
+	"github.com/playmakerchain/thor/builtin"
+	"github.com/playmakerchain/thor/chain"
+	"github.com/playmakerchain/thor/genesis"
+	"github.com/playmakerchain/thor/lvldb"
+	"github.com/playmakerchain/thor/runtime"
+	"github.com/playmakerchain/thor/state"
+	"github.com/playmakerchain/thor/thor"
+	"github.com/playmakerchain/thor/tx"
 )
 
 func TestResolvedTx(t *testing.T) {
