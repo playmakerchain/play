74c0b3adae19a9cff9e82cd8ce1de08288cb10a4
realchainlife <realchainlife@protonmail.com>
2019-04-12 15:18:37 +0300

    modify: import linker >>> playmakerchain

--- trie/sync.go
+++ trie/sync.go
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/vechain//"
+	"github.com/vechain/powerplay/powerplay"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
 
@@ -34,7 +34,7 @@ var ErrAlreadyProcessed = errors.New("already processed")
 
 // request represents a scheduled or already in-flight state retrieval request.
 type request struct {
-	hash .Bytes32 // Hash of the node data content to retrieve
+	hash powerplay.Bytes32 // Hash of the node data content to retrieve
 	data []byte       // Data content of the node, cached until all subtrees complete
 	raw  bool         // Whether this is a raw entry (code) or a trie node
 
@@ -48,29 +48,29 @@ type request struct {
 // SyncResult is a simple list to return missing nodes along with their request
 // hashes.
 type SyncResult struct {
-	Hash .Bytes32 // Hash of the originally unknown trie node
+	Hash powerplay.Bytes32 // Hash of the originally unknown trie node
 	Data []byte       // Data content of the retrieved node
 }
 
 // syncMemBatch is an in-memory buffer of successfully downloaded but not yet
 // persisted data items.
 type syncMemBatch struct {
-	batch map[.Bytes32][]byte // In-memory membatch of recently completed items
-	order [].Bytes32          // Order of completion to prevent out-of-order data loss
+	batch map[powerplay.Bytes32][]byte // In-memory membatch of recently completed items
+	order []powerplay.Bytes32          // Order of completion to prevent out-of-order data loss
 }
 
 // newSyncMemBatch allocates a new memory-buffer for not-yet persisted trie nodes.
 func newSyncMemBatch() *syncMemBatch {
 	return &syncMemBatch{
-		batch: make(map[.Bytes32][]byte),
-		order: make([].Bytes32, 0, 256),
+		batch: make(map[powerplay.Bytes32][]byte),
+		order: make([]powerplay.Bytes32, 0, 256),
 	}
 }
 
 // TrieSyncLeafCallback is a callback type invoked when a trie sync reaches a
 // leaf node. It's used by state syncing to check if the leaf node requires some
 // further data syncing.
-type TrieSyncLeafCallback func(leaf []byte, parent .Bytes32) error
+type TrieSyncLeafCallback func(leaf []byte, parent powerplay.Bytes32) error
 
 // TrieSync is the main state trie synchronisation scheduler, which provides yet
 // unknown trie hashes to retrieve, accepts node data associated with said hashes
@@ -78,24 +78,24 @@ type TrieSyncLeafCallback func(leaf []byte, parent .Bytes32) error
 type TrieSync struct {
 	database DatabaseReader            // Persistent database to check for existing entries
 	membatch *syncMemBatch             // Memory buffer to avoid frequest database writes
-	requests map[.Bytes32]*request // Pending requests pertaining to a key hash
+	requests map[powerplay.Bytes32]*request // Pending requests pertaining to a key hash
 	queue    *prque.Prque              // Priority queue with the pending requests
 }
 
 // NewTrieSync creates a new trie data download scheduler.
-func NewTrieSync(root .Bytes32, database DatabaseReader, callback TrieSyncLeafCallback) *TrieSync {
+func NewTrieSync(root powerplay.Bytes32, database DatabaseReader, callback TrieSyncLeafCallback) *TrieSync {
 	ts := &TrieSync{
 		database: database,
 		membatch: newSyncMemBatch(),
-		requests: make(map[.Bytes32]*request),
+		requests: make(map[powerplay.Bytes32]*request),
 		queue:    prque.New(),
 	}
-	ts.AddSubTrie(root, 0, .Bytes32{}, callback)
+	ts.AddSubTrie(root, 0, powerplay.Bytes32{}, callback)
 	return ts
 }
 
 // AddSubTrie registers a new trie to the sync code, rooted at the designated parent.
-func (s *TrieSync) AddSubTrie(root .Bytes32, depth int, parent .Bytes32, callback TrieSyncLeafCallback) {
+func (s *TrieSync) AddSubTrie(root powerplay.Bytes32, depth int, parent powerplay.Bytes32, callback TrieSyncLeafCallback) {
 	// Short circuit if the trie is empty or already known
 	if root == emptyRoot {
 		return
@@ -115,7 +115,7 @@ func (s *TrieSync) AddSubTrie(root .Bytes32, depth int, parent .Bytes32, callbac
 		callback: callback,
 	}
 	// If this sub-trie has a designated parent, link them together
-	if parent != (.Bytes32{}) {
+	if parent != (powerplay.Bytes32{}) {
 		ancestor := s.requests[parent]
 		if ancestor == nil {
 			panic(fmt.Sprintf("sub-trie ancestor not found: %x", parent))
@@ -130,7 +130,7 @@ func (s *TrieSync) AddSubTrie(root .Bytes32, depth int, parent .Bytes32, callbac
 // interpreted as a trie node, but rather accepted and stored into the database
 // as is. This method's goal is to support misc state metadata retrievals (e.g.
 // contract code).
-func (s *TrieSync) AddRawEntry(hash .Bytes32, depth int, parent .Bytes32) {
+func (s *TrieSync) AddRawEntry(hash powerplay.Bytes32, depth int, parent powerplay.Bytes32) {
 	// Short circuit if the entry is empty or already known
 	if hash == emptyState {
 		return
@@ -148,7 +148,7 @@ func (s *TrieSync) AddRawEntry(hash .Bytes32, depth int, parent .Bytes32) {
 		depth: depth,
 	}
 	// If this sub-trie has a designated parent, link them together
-	if parent != (.Bytes32{}) {
+	if parent != (powerplay.Bytes32{}) {
 		ancestor := s.requests[parent]
 		if ancestor == nil {
 			panic(fmt.Sprintf("raw-entry ancestor not found: %x", parent))
@@ -160,10 +160,10 @@ func (s *TrieSync) AddRawEntry(hash .Bytes32, depth int, parent .Bytes32) {
 }
 
 // Missing retrieves the known missing nodes from the trie for retrieval.
-func (s *TrieSync) Missing(max int) [].Bytes32 {
-	requests := [].Bytes32{}
+func (s *TrieSync) Missing(max int) []powerplay.Bytes32 {
+	requests := []powerplay.Bytes32{}
 	for !s.queue.Empty() && (max == 0 || len(requests) < max) {
-		requests = append(requests, s.queue.PopItem().(.Bytes32))
+		requests = append(requests, s.queue.PopItem().(powerplay.Bytes32))
 	}
 	return requests
 }
@@ -292,7 +292,7 @@ func (s *TrieSync) children(req *request, object node) ([]*request, error) {
 		// If the child references another node, resolve or schedule
 		if node, ok := (child.node).(hashNode); ok {
 			// Try to resolve the node from the local database
-			hash := .BytesToBytes32(node)
+			hash := powerplay.BytesToBytes32(node)
 			if _, ok := s.membatch.batch[hash]; ok {
 				continue
 			}
