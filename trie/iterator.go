e9b4d6ae461d52de3b080cf877fc28fe3eebc112
realchainlife <realchainlife@protonmail.com>
2019-04-12 15:04:18 +0300

    modify: import linker >>> playmakerchain

--- trie/iterator.go
+++ trie/iterator.go
@@ -21,7 +21,7 @@ import (
 	"container/heap"
 	"errors"
 
-	"github.com/vechain/powerplay/powerplay"
+	"github.com//powerplay/powerplay"
 )
 
 // Iterator is a key-value trie iterator that traverses a Trie.
