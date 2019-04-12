b1f64046a29a65db56c388d4ac647ef8c9f7a90c
realchainlife <realchainlife@protonmail.com>
2019-04-12 14:57:57 +0300

    modify: import linker >>> playmakerchain

--- trie/errors.go
+++ trie/errors.go
@@ -19,7 +19,7 @@ package trie
 import (
 	"fmt"
 
-	"github.com//powerplay/powerplay"
+	"github.com/playmakerchain/powerplay/powerplay"
 )
 
 // MissingNodeError is returned by the trie functions (TryGet, TryUpdate, TryDelete)
