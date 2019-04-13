1af31d171f19e6b8953761dae21e4ac49a523b31
realchainlife <realchainlife@protonmail.com>
2019-04-13 17:21:53 +0300

    add playmaker copyright listing

--- packer/packer_test.go
+++ packer/packer_test.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2018 The VeChainThor developers
+// COpyright (c) 2019 The PlayMaker developers
 
 // Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
 // file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
