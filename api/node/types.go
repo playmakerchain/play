f37abd82ce603abf96be2ed2f1da881b20efde58
realchainlife <realchainlife@protonmail.com>
2019-04-14 13:39:55 +0300

    edit indentation format

--- api/node/types.go
+++ api/node/types.go
@@ -16,13 +16,13 @@ type Network interface {
 }
 
 type PeerStats struct {
-	Name        string       `json:"name"`
-	BestBlockID powerplay.Bytes32 `json:"bestBlockID"`
-	TotalScore  uint64       `json:"totalScore"`
-	PeerID      string       `json:"peerID"`
-	NetAddr     string       `json:"netAddr"`
-	Inbound     bool         `json:"inbound"`
-	Duration    uint64       `json:"duration"`
+	Name        string       		`json:"name"`
+	BestBlockID powerplay.Bytes32 	`json:"bestBlockID"`
+	TotalScore  uint64       		`json:"totalScore"`
+	PeerID      string       		`json:"peerID"`
+	NetAddr     string       		`json:"netAddr"`
+	Inbound     bool         		`json:"inbound"`
+	Duration    uint64       		`json:"duration"`
 }
 
 func ConvertPeersStats(ss []*comm.PeerStats) []*PeerStats {
