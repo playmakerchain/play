8e057cb6e6bd3c6db8348d8ebb31648a0301f5f3
realchainlife <realchainlife@protonmail.com>
2019-04-15 10:19:15 +0300

    modify: import linker >>> playmakerchain

--- tracers/tracers.go
+++ tracers/tracers.go
@@ -21,7 +21,7 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com//powerplay/tracers/internal/tracers"
+	"github.com/playmakerchain/powerplay/tracers/internal/tracers"
 )
 
 // all contains all the built in JavaScript tracers by name.
