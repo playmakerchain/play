206ba424dc7e978ef2fadd63fe1d32e17cafa919
realchainlife <realchainlife@protonmail.com>
2019-04-15 10:18:26 +0300

    modify: import linker >>> powerplay

--- tracers/tracers.go
+++ tracers/tracers.go
@@ -21,7 +21,7 @@ import (
 	"strings"
 	"unicode"
 
-	"github.com/vechain//tracers/internal/tracers"
+	"github.com/vechain/powerplay/tracers/internal/tracers"
 )
 
 // all contains all the built in JavaScript tracers by name.
